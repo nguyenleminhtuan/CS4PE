05ebe6e6e93eb2cb7bc69f7df472be705257745b
khangpham <singbad1998@gmail.com>
2018-12-05 12:10:18 +0700

    test

---
+++ DMQuyetTien.Tests/Controllers/BangSanPhamTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication;
+using DMQuyetTien.Models;
+using DMQuyetTien.Controllers;
+
+namespace WebApplication.Tests.Controllers {
+    [TestClass]
+    public class BangSanPhamTest {
+        [TestMethod]
+        public void TestIndex() {
+            var controller = new BangSanPhamController();
+            var result = controller.Index() as ViewResult;
+            var db = new CS4PEEntities();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Model, typeof(List<BangSanPham>));
+            Assert.AreEqual(db.BangSanPhams.Count(), ((List<BangSanPham>)result.Model).Count);
+        }
+    }
+}
--- DMQuyetTien/Controllers/BangSanPhamController.cs
+++ DMQuyetTien/Controllers/BangSanPhamController.cs
@@ -77,36 +77,39 @@ namespace DMQuyetTien.Controllers
                 ModelState.AddModelError("GiaGoc", "Gia gop phai lon hon gia goc");
         }
         // GET: /BangSanPham/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            BangSanPham bangsanpham = db.BangSanPhams.Find(id);
-            if (bangsanpham == null)
-            {
+        public ActionResult Edit(int id) {
+            BangSanPham model = db.BangSanPhams.Find(id);
+            if (model == null) {
                 return HttpNotFound();
             }
-            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai", bangsanpham.Loai_id);
-            return View(bangsanpham);
+            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai", model.Loai_id);
+            return View(model);
         }
-
         // POST: /BangSanPham/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="id,MaSP,TenSP,Loai_id,GiaBan,GiaGoc,GiaGop,SoLuongTon")] BangSanPham bangsanpham)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(bangsanpham).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+        public ActionResult Edit(BangSanPham model) {
+            CheckBangSanPham(model);
+            if (ModelState.IsValid) {
+                using (var scope = new TransactionScope()) {
+                    db.Entry(model).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                    var path = Server.MapPath("~/App_Data");
+                    path = path + "/" + model.id;
+                    if (Request.Files["HinhAnh"] != null &&
+                        Request.Files["HinhAnh"].ContentLength > 0) {
+                        Request.Files["HinhAnh"].SaveAs(path);
+                    }
+
+                    scope.Complete(); // approve for transaction
+                    return RedirectToAction("Index");
+                }
             }
-            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai", bangsanpham.Loai_id);
-            return View(bangsanpham);
+            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai", model.Loai_id);
+            return View(model);
         }
 
         // GET: /BangSanPham/Delete/5
